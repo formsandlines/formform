c0ef135c4565ade2eb3e52f918ea8e72a813f073
Peter Hofmann <peter.hofmann@formsandlines.eu>
2025-06-09 23:58:39 +0200

    emul: refactored emul.test namespace, all tests passed

--- test/formform/emul_test.clj
+++ test/formform/emul_test.clj
@@ -55,29 +55,26 @@
   (let [snapshot-evol (mapv :data snapshots)
         {:keys [data size]} (snapshots 0)
         ini (make-ini :figure :N data {})
-        species (condp = species-type
-                  :lifeform (make-species species-type dna)
-                  :decisionform (make-species species-type dna 10)
-                  :selfi (make-species species-type dna ini)
-                  :mindform (make-species species-type dna ini)
-                  :else (throw (ex-info "Invalid species type."
-                                        {:species species-type})))
-        ca (apply specify-ca species
-                  (if (#{:lifeform :decisionform} species-type)
-                    ;; these species have no ini params, so we overwrite:
-                    {:overwrites {:ini-spec ini}}
-                    {})
-                  size)]
-    [snapshot-evol ca]))
+        ca-spec
+        (condp = species-type
+          :lifeform (make-lifeform {:overwrites {:ini-spec ini}}
+                                   dna 0.5)
+          :decisionform (make-decisionform {:overwrites {:ini-spec ini}}
+                                           dna 0.5 10)
+          :selfi (make-selfi dna ini)
+          :mindform (make-mindform dna ini)
+          :else (throw (ex-info "Invalid species type."
+                                {:species species-type})))]
+    [snapshot-evol ca-spec size]))
 
 (defn equiv-iterator-evolution?
-  [evol ca]
-  (let [iterator (ca-iterator ca)]
+  [evol ca res]
+  (let [iterator (ca-iterator ca res)]
     (= (take (count evol) iterator) evol)))
 
 (defn equiv-automaton-evolution?
-  [evol ca]
-  (let [automaton (create-ca ca (count evol))]
+  [evol ca res]
+  (let [automaton (create-ca ca (count evol) res)]
     (dotimes [_ (count (rest evol))] (step automaton))
     (= (get-cached-history automaton false)
        evol)))
@@ -85,7 +82,7 @@
 ;; alternative method:
 #_
 (defn equiv-automaton-evolution?
-  [evol ca]
+  ca-data
   (let [automaton (create-ca ca 0)
         init-gen (get-current-generation automaton false)]
     (= (cons init-gen (repeatedly (count (rest evol))
@@ -97,99 +94,99 @@
 
 (deftest mindform-congruence
   (testing "100000mindFORM1 snapshot congruence with iterator"
-    (let [[evol ca] (snapshots->ca :mindform
-                                   (mindform-snaps :100000mindFORM1-0000s)
-                                   (tsds-sel->dna [1 0 0 0 0 0]))]
-      (is (equiv-iterator-evolution? evol ca))
-      (is (equiv-automaton-evolution? evol ca)))
+    (let [ca-data (snapshots->ca :mindform
+                                 (mindform-snaps :100000mindFORM1-0000s)
+                                 (tsds-sel->dna [1 0 0 0 0 0]))]
+      (is (apply equiv-iterator-evolution? ca-data))
+      (is (apply equiv-automaton-evolution? ca-data)))
 
     ;; tests should pass, but left out since they’re too slow:
     #_
-    (let [[evol ca] (snapshots->ca :mindform
-                                   (mindform-snaps :100000mindFORM1-0036s)
-                                   (tsds-sel->dna [1 0 0 0 0 0]))]
-      (is (equiv-iterator-evolution? evol ca))
-      (is (equiv-automaton-evolution? evol ca))))
+    (let [ca-data (snapshots->ca :mindform
+                                 (mindform-snaps :100000mindFORM1-0036s)
+                                 (tsds-sel->dna [1 0 0 0 0 0]))]
+      (is (apply equiv-iterator-evolution? ca-data))
+      (is (apply equiv-automaton-evolution? ca-data))))
 
   (testing "100101mindFORM snapshot congruence with iterator"
-    (let [[evol ca] (snapshots->ca :mindform
-                                   (mindform-snaps :100101mindFORM_0000s)
-                                   (tsds-sel->dna [1 0 0 1 0 1]))]
-      (is (equiv-iterator-evolution? evol ca))
-      (is (equiv-automaton-evolution? evol ca)))
+    (let [ca-data (snapshots->ca :mindform
+                                 (mindform-snaps :100101mindFORM_0000s)
+                                 (tsds-sel->dna [1 0 0 1 0 1]))]
+      (is (apply equiv-iterator-evolution? ca-data))
+      (is (apply equiv-automaton-evolution? ca-data)))
 
     ;; tests should pass, but left out since they’re too slow:
     #_
-    (let [[evol ca] (snapshots->ca :mindform
-                                   (mindform-snaps :100101mindFORM_0015s)
-                                   (tsds-sel->dna [1 0 0 1 0 1]))]
-      (is (equiv-iterator-evolution? evol ca))
-      (is (equiv-automaton-evolution? evol ca)))))
+    (let [ca-data (snapshots->ca :mindform
+                                 (mindform-snaps :100101mindFORM_0015s)
+                                 (tsds-sel->dna [1 0 0 1 0 1]))]
+      (is (apply equiv-iterator-evolution? ca-data))
+      (is (apply equiv-automaton-evolution? ca-data)))))
 
 (deftest lifeform-congruence
   (testing "100101lifeFORMs_0000s snapshot congruence with iterator"
-    (let [[evol ca] (snapshots->ca :lifeform
-                                   (lifeform-snaps :100101lifeFORMs_0000s)
-                                   (tsds-sel->dna [1 0 0 1 0 1]))]
-      (is (equiv-iterator-evolution? evol ca))
-      (is (equiv-automaton-evolution? evol ca))))
+    (let [ca-data (snapshots->ca :lifeform
+                                 (lifeform-snaps :100101lifeFORMs_0000s)
+                                 (tsds-sel->dna [1 0 0 1 0 1]))]
+      (is (apply equiv-iterator-evolution? ca-data))
+      (is (apply equiv-automaton-evolution? ca-data))))
 
   (testing "101101circulator_0000s snapshot congruence with iterator"
-    (let [[evol ca] (snapshots->ca :lifeform
-                                   (lifeform-snaps :101101circulator_0000s)
-                                   ;; the TSDS is NOT 101101 but 100101!
-                                   (tsds-sel->dna [1 0 0 1 0 1]))]
-      (is (equiv-iterator-evolution? evol ca))
-      (is (equiv-automaton-evolution? evol ca)))))
+    (let [ca-data (snapshots->ca :lifeform
+                                 (lifeform-snaps :101101circulator_0000s)
+                                 ;; the TSDS is NOT 101101 but 100101!
+                                 (tsds-sel->dna [1 0 0 1 0 1]))]
+      (is (apply equiv-iterator-evolution? ca-data))
+      (is (apply equiv-automaton-evolution? ca-data)))))
 
 (deftest decisionform-congruence
   (testing "100101decisionFORMs_0000s snapshot congruence with iterator"
-    (let [[evol ca] (snapshots->ca :decisionform
-                                   (decisionform-snaps
-                                    :100101decisionFORMs_0000s)
-                                   (tsds-sel->dna [1 0 0 1 0 1]))]
-      (is (equiv-iterator-evolution? evol ca))
-      (is (equiv-automaton-evolution? evol ca)))))
+    (let [ca-data (snapshots->ca :decisionform
+                                 (decisionform-snaps
+                                  :100101decisionFORMs_0000s)
+                                 (tsds-sel->dna [1 0 0 1 0 1]))]
+      (is (apply equiv-iterator-evolution? ca-data))
+      (is (apply equiv-automaton-evolution? ca-data)))))
 
 (deftest selfi-congruence
   (testing "Mark1 snapshot congruence"
     (is (= (selfi-snaps :Mark1-r25t20) ;; roughly verified
-           (take 20 (ca-iterator (specify-ca (common-specimen :Mark1)
-                                             {} 25))))))
+           (take 20 (ca-iterator (common-specimen :Mark1)
+                                 [25])))))
   (testing "Slit snapshot congruence"
     (is (= (selfi-snaps :Slit-r27t20) ;; roughly verified
-           (take 20 (ca-iterator (specify-ca (common-specimen :Slit)
-                                             {} 27))))))
+           (take 20 (ca-iterator (common-specimen :Slit)
+                                 [27])))))
 
   (testing "or4v snapshot congruence"
     (is (= (selfi-snaps :or4v-r21t19) ;; fully verified
-           (take 19 (ca-iterator (specify-ca (common-specimen :or4v)
-                                             {} 21))))))
+           (take 19 (ca-iterator (common-specimen :or4v)
+                                 [21])))))
 
   (testing "Rule4v110 snapshot congruence"
     (is (= (selfi-snaps :xorReId-r27t20) ;; fully verified
-           (take 20 (ca-iterator (specify-ca (common-specimen :xorReId)
-                                             {} 27))))))
+           (take 20 (ca-iterator (common-specimen :xorReId)
+                                 [27])))))
 
   (testing "Rule4v30 snapshot congruence"
     (is (= (selfi-snaps :Rule4v30-r27t11) ;; fully verified
-           (take 11 (ca-iterator (specify-ca (common-specimen :Rule4v30)
-                                             {} 27))))))
+           (take 11 (ca-iterator (common-specimen :Rule4v30)
+                                 [27])))))
 
   (testing "Rule4v111 snapshot congruence"
     (is (= (selfi-snaps :Rule4v111-r19t19) ;; fully verified
-           (take 19 (ca-iterator (specify-ca (common-specimen :Rule4v111)
-                                             {} 19))))))
+           (take 19 (ca-iterator (common-specimen :Rule4v111)
+                                 [19])))))
 
   (testing "Structure111Re snapshot congruence"
     (is (= (selfi-snaps :Structure111Re-r27t20) ;; roughly verified
-           (take 20 (ca-iterator (specify-ca (common-specimen :Structure111Re)
-                                             {} 27))))))
+           (take 20 (ca-iterator (common-specimen :Structure111Re)
+                                 [27])))))
 
   (testing "Rule4v110 snapshot congruence"
     (is (= (selfi-snaps :Rule4v110-r25t20) ;; fully verified
-           (take 20 (ca-iterator (specify-ca (common-specimen :Rule4v110)
-                                             {} 25)))))))
+           (take 20 (ca-iterator (common-specimen :Rule4v110)
+                                 [25]))))))
 
 
 
@@ -199,35 +196,35 @@
 
   (binding [pprint/*print-right-margin* 80
             pprint/*print-miser-width* 80]
-    (pprint (take 20 (ca-iterator (specify-ca (common-specimen :Mark1)
-                                              {} 25)))))
+    (pprint (take 20 (ca-iterator (common-specimen :Mark1)
+                                  [25]))))
 
   ,)
 
 (comment
 
-  (let [[evol ca] (snapshots->ca :lifeform
-                                 (lifeform-snaps :101101circulator_0000s)
-                                 (tsds-sel->dna [1 0 0 1 0 1]))]
-    (equiv-iterator-evolution? evol ca))
+  (let [ca-data (snapshots->ca :lifeform
+                               (lifeform-snaps :101101circulator_0000s)
+                               (tsds-sel->dna [1 0 0 1 0 1]))]
+    (apply equiv-iterator-evolution? ca-data))
 
   
-  (let [[evol ca] (snapshots->ca :lifeform
-                                 (lifeform-snaps :100101lifeFORMs_0000s)
-                                 (tsds-sel->dna [1 0 0 1 0 1]))]
-    ;; (equiv-iterator-evolution? evol ca)
-    (equiv-automaton-evolution? evol ca))
+  (let [ca-data (snapshots->ca :lifeform
+                               (lifeform-snaps :100101lifeFORMs_0000s)
+                               (tsds-sel->dna [1 0 0 1 0 1]))]
+    ;; (apply equiv-iterator-evolution? ca-data)
+    (apply equiv-automaton-evolution? ca-data))
 
   (def snapshot ((lifeform-snaps :100101lifeFORMs_0000s) 0))
   (def ini-gen (:data snapshot))
   (def res (:size snapshot))
 
   (= (->> (ca-iterator
-           (apply specify-ca
-                  (make-species :lifeform (tsds-sel->dna [1 0 0 1 0 1]))
-                  {:overwrites {:ini-spec
-                                (make-ini :fill-all ini-gen)}}
-                  res))
+           (make-lifeform {:overwrites {:ini-spec
+                                        (make-ini :fill-all ini-gen)}}
+                          (tsds-sel->dna [1 0 0 1 0 1]))
+                  
+           [res])
           (take 10))
      (map :data (lifeform-snaps :100101lifeFORMs_0000s)))
 
@@ -237,31 +234,28 @@
     (some
      (fn [perm]
        (= (->> (ca-iterator
-                (apply specify-ca
-                       (make-species :lifeform (tsds-sel->dna perm))
-                       {:overwrites {:ini-spec
-                                     (make-ini :fill-all ini-gen)}}
-                       res))
+                (make-lifeform {:overwrites {:ini-spec
+                                             (make-ini :fill-all ini-gen)}}
+                               (tsds-sel->dna perm))
+                [res])
                (take 2)
                last)
           (:data ((lifeform-snaps :100101lifeFORMs_0000s) 1))))
      perms))
 
   
-  (let [[evol ca] (snapshots->ca :mindform
-                                 (mindform-snaps :100000mindFORM1-0000s)
-                                 (tsds-sel->dna [1 0 0 0 0 0]))]
-    (equiv-automaton-evolution? evol ca))
+  (let [ca-data (snapshots->ca :mindform
+                               (mindform-snaps :100000mindFORM1-0000s)
+                               (tsds-sel->dna [1 0 0 0 0 0]))]
+    (apply equiv-automaton-evolution? ca-data))
 
-  (def atm (create-ca
-            (specify-ca (make-species :selfi
-                                      [:N :N :N :N
-                                       :N :I :N :I
-                                       :I :I :I :I
-                                       :I :U :I :U]
-                                      (make-ini :ball))
-                        {}
-                        10)))
+  (def atm (create-ca (make-species :selfi
+                                    [:N :N :N :N
+                                     :N :I :N :I
+                                     :I :I :I :I
+                                     :I :U :I :U]
+                                    (make-ini :ball))
+                      [10]))
   (get-system-time atm)
   (get-resolution atm)
   (step atm)
